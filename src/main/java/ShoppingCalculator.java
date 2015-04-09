41ce338311c74afda5fe2361ffacd1d6a751cf1c
beckfordp <beckfordp@btinternet.com>
2015-04-09 07:12:43 +0100

    Made ShoppingCalculator object immutable.

--- src/main/java/ShoppingCalculator.java
+++ src/main/java/ShoppingCalculator.java
@@ -6,8 +6,10 @@ import java.util.List;
  * Calculates the Price of a basket of items.
  * A functional reactive solution using Java 8 and lambdas.
  * I thought I'd give a reactive solution a go, since it felt like a natural fit.
- * Also a reactive solution should prove more amenable should the need arise
- * to deal with a large number of baskets, and/or calculate a running total as we shop.
+ *
+ * A reactive solution is more amenable should the need arise
+ * to handle a very large basket or a large number of baskets,
+ * and/or calculate a running total as we shop.
  */
 public class ShoppingCalculator {
     public static final int APPLE_PRICE_EACH = 35;
@@ -15,16 +17,17 @@ public class ShoppingCalculator {
     public static final int MELON_PRICE_EACH = 50;
     public static final int LIME_PRICE_EACH = 15;
 
-    Observable<String> basketObservable;
+    private Observable<String> basketObservable;
+    private Observable<Integer> totalPriceObservable;
 
     public ShoppingCalculator(List<String> items) {
         basketObservable = Observable.from(items);
+        totalPriceObservable = Observable.merge(applePriceEach(), bananaPriceEach(), melonPriceOffer(), limePriceOffer())
+                                        .startWith(0).scan(this::sum);
     }
 
     public Observable<Integer> totalPrice() {
-        Observable<Integer> itemPriceObservable =
-            Observable.merge(applePriceEach(), bananaPriceEach(), melonPriceOffer(), limePriceOffer()).startWith(0);
-        return itemPriceObservable.scan(this::sum);
+        return totalPriceObservable;
     }
 
     private Observable<Integer> applePriceEach() {
@@ -41,7 +44,7 @@ public class ShoppingCalculator {
 
     private Observable<Integer> limePriceOffer() {
         return basketObservable.filter("Lime"::equals).buffer(3)
-                               .map((eachOffer) -> eachOffer.size() == 3 ? LIME_PRICE_EACH * 2 : LIME_PRICE_EACH * eachOffer.size() );
+                               .map((eachOffer) -> eachOffer.size() == 3 ? LIME_PRICE_EACH * 2 : LIME_PRICE_EACH * eachOffer.size());
     }
 
     private Integer sum(Integer acc, Integer next) {
