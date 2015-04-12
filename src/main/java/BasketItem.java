12ad0f943a9469bf064fd8619fba81c205ee956a
beckfordp <beckfordp@btinternet.com>
2015-04-12 13:05:16 +0100

    Added GenericCalculator for BasketItems.

---
+++ src/main/java/BasketItem.java
@@ -0,0 +1,28 @@
+
+public class BasketItem {
+    private String name;
+    private OfferEnum offer;
+    private Integer unitPrice;
+
+    public static BasketItem Apple = new BasketItem("Apple", OfferEnum.NONE, 35);
+    public static BasketItem Banana = new BasketItem("Banana", OfferEnum.NONE, 20);
+    public static BasketItem Melon = new BasketItem("Melon", OfferEnum.BUY_ONE_GET_ONE_FREE, 50);
+    public static BasketItem Lime = new BasketItem("Lime", OfferEnum.BUY_THREE_FOR_THE_PRICE_OF_TWO, 15);
+
+    public BasketItem(String name, OfferEnum offer, Integer unitPrice) {
+         this.name = name; this.offer = offer; this.unitPrice = unitPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public OfferEnum getOffer() {
+        return offer;
+    }
+
+    public Integer getUnitPrice() {
+        return unitPrice;
+    }
+
+}
